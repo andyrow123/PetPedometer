4ded3171836c29f2fd388810ead03dfd8126c9bd
unknown <Administrator@BUDFARM.INTERNAL>
2021-03-17 22:59:23 +0000

    Initial solution

---
+++ Source/PetPedometer.Core/IClientApplicationSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// The core functionality shared between all parts of the PetPedometer application
+/// </summary>
+namespace PetPedometer.Core
+{
+    /// <summary>
+    /// The entry point into the PetPedometer application used to setup the environment.
+    /// All top-level applications should create an instance of this
+    /// </summary>
+    public interface IClientApplicationSetup
+    {
+
+    }
+}
